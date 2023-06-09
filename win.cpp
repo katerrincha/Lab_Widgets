f07bc9bfa245f1c9721213cbe8ea6222a50feb6e
mrouw <bchva@yandex.ru>
2023-06-09 12:51:27 +0700

    changes

--- win.cpp
+++ win.cpp
@@ -34,7 +34,7 @@ Win::Win(QWidget *parent): QWidget(parent)
     layout4->addLayout(layout3);
 
     // связь сигнала нажатия кнопки и слота закрытия окна
-    connect(calcbutton, SIGNAL(clicked(bool)), edit1, SLOT(add_one()));
-    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one()));
-    connect(exitbutton, SIGNAL(clicked(bool)), this, SLOT(close()));
+    connect(calcbutton, &QPushButton::clicked, edit1, &Counter::add_one);
+    connect(edit1, &Counter::tick_signal, edit2, &Counter::add_one);
+    connect(exitbutton, &QPushButton::clicked, this, &Win::close);
 }
